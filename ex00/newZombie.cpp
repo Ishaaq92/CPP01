c9ded268a45de4b416ee3628c264a8f5cf841d3f
Ishaaq Ahmed <a.ishaaq@icloud.com>
2025-07-26 23:09:52 +0100

    Realised that I didn't create all the right files to be turned in

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/26 22:38:00 by ishaaq            #+#    #+#             */
-/*   Updated: 2025/07/26 23:06:16 by ishaaq           ###   ########.fr       */
+/*   Created: 2025/07/26 23:05:50 by ishaaq            #+#    #+#             */
+/*   Updated: 2025/07/26 23:05:51 by ishaaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie*	newZombie(std::string name);
-void	randomChump(std::string name);
-
-int main(void)
+Zombie*	newZombie(std::string name)
 {
-    Zombie  *z;
+	Zombie	*z;
 
-    randomChump("Timmy");
-    z = newZombie("Sam");
-    z->announce();
-    delete z;
+	z = new Zombie;
+	z->set_name(name);
+	return (z);
 }
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/26 22:38:00 by ishaaq            #+#    #+#             */
-/*   Updated: 2025/07/26 23:06:16 by ishaaq           ###   ########.fr       */
+/*   Created: 2025/07/26 23:05:29 by ishaaq            #+#    #+#             */
+/*   Updated: 2025/07/26 23:05:30 by ishaaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie*	newZombie(std::string name);
-void	randomChump(std::string name);
-
-int main(void)
+void	randomChump(std::string name)
 {
-    Zombie  *z;
+	Zombie	z;
 
-    randomChump("Timmy");
-    z = newZombie("Sam");
-    z->announce();
-    delete z;
+	z.set_name(name);
+	z.announce();
+	return ;
 }
