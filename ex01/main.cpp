bf103776be49221db95560f1f1585546c01135ea
ishaaq <a.ishaaq@icloud.com>
2025-08-28 14:38:27 +0100

    Works well. no leaks. Fixed makefile.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 22:38:00 by ishaaq            #+#    #+#             */
-/*   Updated: 2025/07/27 10:06:55 by ishaaq           ###   ########.fr       */
+/*   Updated: 2025/08/28 14:36:27 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ Zombie*	zombieHorde(int N, std::string name);
 int main(void)
 {
 	Zombie  *z;
+	int		N;
 
-	z = zombieHorde(5, "Timmy");
-	z[0].announce();
+	N = 5;
+
+	z = zombieHorde(N, "Timmy");
+	for (int i=0;i < N;i++)
+		z[i].announce();
 	delete[] z;
 	return (0);
 }
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 23:05:50 by ishaaq            #+#    #+#             */
-/*   Updated: 2025/07/27 10:03:34 by ishaaq           ###   ########.fr       */
+/*   Updated: 2025/08/28 14:32:08 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ Zombie*	zombieHorde(int N, std::string name)
 {
 	Zombie	*z;
 
+	if (N < 1)
+		return (NULL);
 	z = new Zombie[N];
 	for (int i=0;i < N;i++)
 		z[i].set_name(name);
 	return (z);
 }
+
