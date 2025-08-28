81f0cfe247751d9ad62fe03c8f6dea59fdd3d6b1
ishaaq <a.ishaaq@icloud.com>
2025-08-28 16:46:32 +0100

    Looking a lot better. Functional. Now it actually creates and writes to the correct file. Adds the '.replace' suffix

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 
 std::string	freplace(const std::string& text, const std::string& s1, const std::string s2)
 {
@@ -12,11 +13,23 @@ std::string	freplace(const std::string& text, const std::string& s1, const std::
 	return (text.substr(0, index) + s2 + freplace(text.substr(index + s1.length(), text.length()), s1, s2));
 }
 
+void	writeToFile(std::string fileName, std::string s)
+{
+	// std::ofstream	newFile(fileName.append(".replace"));
+
+	fileName.append(".replace");
+	std::cout <<fileName<< std::endl;
+	std::ofstream	MyFile(fileName.c_str());
+
+	MyFile << s;
+	MyFile.close();
+}
+
 int	main(int ac, char **av)
 {
 	std::string text;
 	std::string tmp;
-	std::string	final;
+	std::string	s;
 
 	if (ac != 4)
 		return (1);
@@ -25,10 +38,12 @@ int	main(int ac, char **av)
 		return (std::cout << "Invalid file" << std::endl, 1);
 	while (getline(read, tmp))
 		text.append(tmp).append("\n");
-	if (text.empty() || text.find(av[2]) == text.npos)
+	if (text.empty())
 		return (std::cout << text << std::endl, 1);
 
-	final = freplace(text, av[2], av[3]);
-	std::cout << final << std::endl;
+	s = freplace(text, av[2], av[3]);
+	// std::cout << final << std::endl;
+	// std::string	file(av[1]);
+	writeToFile(av[1], s);
 	return (0);
 }
